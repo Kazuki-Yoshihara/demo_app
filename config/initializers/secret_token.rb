1309d91df9239623da9922a4561ff5298860e9c7
peta <peta.cirlce9@gmail.com>
2015-03-19 13:38:54 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd1917b82a76a3559bc81ae7c37b15a0293a0cea4082739998eca628779c57237b9f088acd49e924202def5e86317d8ee2356d82c2296b188a271b6d123611c48'
